4375339f44a623ae2941df4fc49c44c4153a74a7
Eduard Urbach <e.urbach@gmail.com>
2016-03-23 11:49:33 +0900

    Fixed page indenting

--- pages/pages/pages.js
+++ pages/pages/pages.js
@@ -1,17 +1,15 @@
 exports.get = (request, response) => {
 	let pages = Array.from(this.app.site.pages.values())
+
 	pages.sort((a, b) => {
 		return a.id.localeCompare(b.id)
 	})
 
 	pages.forEach(page => {
 		page.indent = 0
-		page.opacity = 1.0
 
-		if(page.id.indexOf('/') !== -1) {
-			page.indent = (page.id.match(/\//g) || []).length + 1
-			page.opacity = Math.max(1.0 - page.indent * 0.1, 0.2)
-		}
+		if(page.id.indexOf('/') !== -1)
+			page.indent = (page.id.match(/\//g) || []).length
 	})
 
 	response.render({
