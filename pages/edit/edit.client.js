f56bd2d889aa32cf7e7b2a3392fe442319f9aa61
Eduard Urbach <e.urbach@gmail.com>
2016-03-24 12:41:25 +0900

    Page file listing and editing

--- pages/edit/edit.client.js
+++ pages/edit/edit.client.js
@@ -1,4 +1,48 @@
+const modeNames = {
+	js: 'javascript',
+	jsonld: 'json',
+	styl: 'stylus',
+	md: 'markdown'
+}
+
+let file = $('fileName').textContent
+let fileExtension = file.substr(file.lastIndexOf('.') + 1)
+let modeName = modeNames[fileExtension] || fileExtension
+
+// Needs to be reassigned every time due to closures
+window.saveFile = function() {
+	$.post('/_/edit', {
+		file,
+		contents: editor.getSession().getValue()
+	}).then(() => {
+		alert('File saved.')
+	})
+}
+
+// Resize editor when resizing window
+if(!window.editorEventsRegistered) {
+	window.resizeEditor = function() {
+		$('json-editor').style.height = (window.innerHeight - $('nav').clientHeight) + 'px'
+		editor.resize()
+	}
+
+	window.addEventListener('resize', window.resizeEditor)
+
+	// Capture Ctrl + S event
+	window.addEventListener('keydown', function(e) {
+		if(e.ctrlKey && e.which === 83) {
+	        window.saveFile()
+	        e.preventDefault()
+	        return false
+	    }
+	})
+
+	window.editorEventsRegistered = true
+}
+
+// Init editor
 var editor = ace.edit('json-editor')
+
 editor.setTheme("ace/theme/monokai")
 editor.setOption('showLineNumbers', false)
 editor.setOption('showGutter', false)
@@ -6,18 +50,25 @@ editor.setOption('printMargin', false)
 editor.setOption('useSoftTabs', false)
 editor.setOption('fontSize', '1em')
 
-let file = $('fileName').textContent
-let fileExtension = file.substr(file.lastIndexOf('.') + 1)
-let JSONMode = ace.require(`ace/mode/${fileExtension}`).Mode
-editor.session.setMode(new JSONMode())
+if(window.modesLoaded === undefined)
+	window.modesLoaded = {}
 
-window.saveFile = function() {
-	$.post('/_/edit', {
-		file,
-		contents: editor.getSession().getValue()
-	})
+let updateMode = function() {
+	let JSONMode = ace.require(`ace/mode/${modeName}`).Mode
+	editor.session.setMode(new JSONMode())
 }
 
-window.addEventListener('keydown', function(e) {
-	// Ctrl S...
-})
+if(window.modesLoaded[modeName]) {
+	updateMode()
+} else {
+	let highlighter = document.createElement('script')
+	highlighter.src = `https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.3/mode-${modeName}.js`
+	highlighter.onload = function() {
+		window.modesLoaded[modeName] = true
+		updateMode()
+	}
+
+	document.head.appendChild(highlighter)
+}
+
+window.resizeEditor()
