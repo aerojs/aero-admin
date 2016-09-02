edd57ecbd438339d1d2322df37368aa054f62dd8
Eduard Urbach <e.urbach@gmail.com>
2016-09-02 11:41:29 +0900

    Added OS version to system info

--- pages/dash/dash.js
+++ pages/dash/dash.js
@@ -1,3 +1,13 @@
+let osVersion = null
+let execWithCallback = require('child_process').exec
+let exec = Promise.promisify((command, callback) => {
+	execWithCallback(command, function(error, stdout, stderr) {
+		callback(error, stdout)
+	})
+})
+
+exec('lsb_release -r -s').then(output => osVersion = output.trim())
+
 exports.get = (request, response) => {
 	let certificateValidity = (new Date(this.app.site.certificate.validity.end)).toISOString()
 	certificateValidity = certificateValidity.substring(0, certificateValidity.indexOf('T'))
@@ -5,7 +15,8 @@ exports.get = (request, response) => {
 	response.render({
 		averageResponseTime: this.app.site.averageResponseTime,
 		averageCodeSize: this.app.site.averageCodeSize,
-		certificateValidity
+		certificateValidity,
+		osVersion
 		// ...
 	})
 }
